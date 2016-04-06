08bc03dee577931fe1c4cb30cb3b84876011763c
keerthireddym <keerthireddym.0910@gmail.com>
2016-04-06 23:06:11 +0530

    counter with limit, highligh Fingers and toes

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-/**
- * Created by vvarikuti on 06/04/16.
- */
+'use strict';
+
+$(document).ready(function () {
+    var limit = $('#limit'),
+        count,
+        counter = $('#counter'), 
+        fingers = $('#fingers'), 
+        toes = $('#toes'), 
+        interval;
+    
+    limit.on('input change', function() {
+        var val = $(this).val();
+        startCounter()
+    })
+    
+    
+    function highlightSections() {
+        fingers.removeClass('highlight');
+        toes.removeClass('highlight');
+        
+        if (count % 3 ==0) {
+            fingers.addClass('highlight');
+        }
+        if (count % 5 ==0) {
+            toes.addClass('highlight');
+        }
+    }
+    
+    function updateCounter() {
+        if (count >= limit.val()) {
+            clearInterval(interval);
+        } else {
+            counter.html(++count);
+            highlightSections();
+        }        
+    }
+    
+    function startCounter() {
+        count = 0;
+        interval = setInterval(updateCounter, 1000);
+    }
+});
