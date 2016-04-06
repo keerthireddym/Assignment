30182ed412bb9d939cb9981de6088adc2350d425
keerthireddym <keerthireddym.0910@gmail.com>
2016-04-06 23:13:45 +0530

    Added Reset Button

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 'use strict';
 
 $(document).ready(function () {
-    var limit = $('#limit'),
-        count,
-        counter = $('#counter'), 
-        fingers = $('#fingers'), 
-        toes = $('#toes'), 
-        interval;
-    
-    limit.on('input change', function() {
-        var val = $(this).val();
-        startCounter()
-    })
-    
-    
-    function highlightSections() {
-        fingers.removeClass('highlight');
-        toes.removeClass('highlight');
-        
-        if (count % 3 ==0) {
-            fingers.addClass('highlight');
-        }
-        if (count % 5 ==0) {
-            toes.addClass('highlight');
-        }
+  var limit = $('#limit'),
+    count,
+    counter = $('#counter'),
+    fingers = $('#fingers'),
+    toes = $('#toes'),
+    interval,
+    reset = $('#reset');
+
+  limit.on('input change', function () {
+    var val = $(this).val();
+    if (val)
+      startCounter()
+  });
+
+  reset.on('click', function () {
+    count = 0;
+    counter.html(count);
+    clearHighlight();
+    clearInterval(interval);
+    limit.val('');
+  });
+
+  function clearHighlight() {
+    fingers.removeClass('highlight');
+    toes.removeClass('highlight');
+  }
+
+  function highlightSections() {
+    clearHighlight();
+
+    if (count % 3 == 0) {
+      fingers.addClass('highlight');
     }
-    
-    function updateCounter() {
-        if (count >= limit.val()) {
-            clearInterval(interval);
-        } else {
-            counter.html(++count);
-            highlightSections();
-        }        
+    if (count % 5 == 0) {
+      toes.addClass('highlight');
     }
-    
-    function startCounter() {
-        count = 0;
-        interval = setInterval(updateCounter, 1000);
+  }
+
+  function updateCounter() {
+    if (count >= limit.val()) {
+      clearInterval(interval);
+    } else {
+      counter.html(++count);
+      highlightSections();
     }
+  }
+
+  function startCounter() {
+    count = 0;
+    interval = setInterval(updateCounter, 1000);
+  }
 });
